097b9933853a06e1c12fb5f56024e5a444c1052b
alashchev17 <andrew.lashchev15@gmail.com>
2025-02-22 18:01:37 +0100

    feat: mobile menu implemented & fixed overflow clip for sections

---
+++ scripts/main.js
@@ -0,0 +1,50 @@
+jQuery(function () {
+  // Register ScrollTrigger plugin
+  gsap.registerPlugin(ScrollTrigger)
+  gsap.registerPlugin(CustomEase)
+
+  const lenis = new Lenis({
+    autoRaf: true,
+  })
+
+  lenis.on('scroll', ScrollTrigger.update)
+
+  gsap.ticker.add((time) => {
+    lenis.raf(time * 1000)
+  })
+
+  gsap.ticker.lagSmoothing(0)
+
+  // Header
+  const body = $('body')
+  const header = $('.header')
+  const menu = $('.header__menu')
+  const burgerButton = $('.header__burger')
+
+  let isMenuOpened = false
+
+  burgerButton.on('click', (event) => {
+    event.preventDefault()
+    body.toggleClass('overflow-hidden')
+
+    if (isMenuOpened) {
+      lenis.start()
+      menu.toggleClass('active')
+      burgerButton.toggleClass('rotate')
+      setTimeout(() => {
+        burgerButton.toggleClass('active')
+        header.toggleClass('active')
+      }, 300)
+    } else {
+      lenis.stop()
+      burgerButton.toggleClass('active')
+      header.toggleClass('active')
+      setTimeout(() => {
+        menu.toggleClass('active')
+        burgerButton.toggleClass('rotate')
+      }, 300)
+    }
+
+    isMenuOpened = !isMenuOpened
+  })
+})
