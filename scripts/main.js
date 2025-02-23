c1331b8ca444ab25c07c62c4645c7ad3bcd152b2
alashchev17 <andrew.lashchev15@gmail.com>
2025-02-23 20:30:46 +0100

    feat: responsive layout for solution, details, advantages sections & slick slider connected

--- scripts/main.js
+++ scripts/main.js
@@ -57,4 +57,90 @@ jQuery(function () {
 
     isMenuOpened = !isMenuOpened
   })
+
+  // Solution slider
+
+  function destroyAndReinitializeSolutionSlider() {
+    const solutionSlider = $('.solution__slider-wrapper')
+    let sliderButtons = {
+      prevArrow: $('.solution__slider-controls--mobile .solution__slider-button--prev'),
+      nextArrow: $('.solution__slider-controls--mobile .solution__slider-button--next'),
+    }
+    if (solutionSlider.hasClass('slick-initialized')) {
+      solutionSlider.slick('destroy')
+    }
+    if ($(window).width() > 768) {
+      // переназначение кнопок управление в слайдер для десктопной версии
+      solutionSlider.slick('destroy')
+    } else {
+      solutionSlider.slick({
+        nextArrow: sliderButtons.nextArrow,
+        prevArrow: sliderButtons.prevArrow,
+        infinite: false,
+        draggable: false,
+        adaptiveHeight: true,
+        swipe: true,
+        swipeToSlide: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 650,
+            settings: {
+              adaptiveHeight: true,
+              swipe: true,
+              swipeToSlide: true,
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              variableWidth: true,
+            },
+          },
+        ],
+      })
+      console.log(solutionSlider.slick('getSlick'))
+    }
+  }
+  function destroyAndReinitializeAdvantagesSlider() {
+    const advantagesSlider = $('.advantages__slider-wrapper')
+    let sliderButtons = {
+      prevArrow: $('.advantages__slider-controls--mobile .advantages__slider-button--prev'),
+      nextArrow: $('.advantages__slider-controls--mobile .advantages__slider-button--next'),
+    }
+    if (advantagesSlider.hasClass('slick-initialized')) {
+      advantagesSlider.slick('destroy')
+    }
+    if ($(window).width() > 768) {
+      // переназначение кнопок управление в слайдер для десктопной версии
+      advantagesSlider.slick('destroy')
+    } else {
+      advantagesSlider.slick({
+        nextArrow: sliderButtons.nextArrow,
+        prevArrow: sliderButtons.prevArrow,
+        infinite: false,
+        draggable: false,
+        adaptiveHeight: true,
+        variableWidth: true,
+        swipe: true,
+        swipeToSlide: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 540,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+            },
+          },
+        ],
+      })
+      console.log(advantagesSlider.slick('getSlick'))
+    }
+  }
+
+  $(window).on('load', () => {
+    // Starting all instances of the landing page
+    destroyAndReinitializeSolutionSlider()
+    destroyAndReinitializeAdvantagesSlider()
+  })
 })
